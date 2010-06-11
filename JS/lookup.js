e1ed18753a0ba49732a7e2ca48c6ca3c347ee184
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2010-06-11 07:55:09 -0400

    more droppables; dropping into hand or cadre unstacks

---
+++ JS/lookup.js
@@ -0,0 +1,37 @@
+var BVGLOOKUP = {};
+
+BVGLOOKUP.cards = {
+    U123: {
+        name: "XIII Corps",
+        side: "usa",
+        type: "inf",
+        str: 4,
+        theater: "West"
+    },
+    U124: { },
+    U125: { },
+    U126: { },
+    U127: { },
+    U128: { },
+    U129: { },
+    U130: { },
+    U131: { },
+    U201: {
+        name: "Grant",
+        side: "usa",
+        type: "ldr",
+        str: 4,
+        ainit: 2,
+        dinit: 2,
+        acap: 2,
+        ccap: 1,
+        scap: 2,
+        politics: "Political Favor",
+        special: "If combat result is Soldier's Battle, Grant always takes (or holds) his city (etc.), even if he doesn't win the battle."
+    },
+    U202: { },
+    U203: { },
+    U204: { },
+    U205: { },
+    U206: { }
+};
--- JS/oob.js
+++ JS/oob.js
@@ -1,30 +1,38 @@
 var draggable;
 
+function dropCard(dropElt, dragElt) {
+    dragElt.remove();
+    dragElt.setStyle( { left: 0, top: 0 } );
+    dropElt.down("ul").appendChild(dragElt);
+    if (dropElt.hasClassName("unstacks")) {
+        dragElt.select("li").each( function(childElt) { dropCard(dropElt, childElt); } );
+    }
+}
+
 function domLoaded() {
-  $A(["cadre", "usaWest"].concat($$(".holdsInf", ".holdsCav", ".holdsLdr"))).each(function(eltName) {
+  // SplitPane and Droppable interfere with each other...
+//  new SplitPane("usaCadre", 16, "usaWest", 16, 16, { active: true } );
+//  new SplitPane("usaWest" , 16, "usaEast", 32, 16, { active: true } );
+
+  $A(["usaHand", "usaWest", "usaEast", "usaCadre"].concat($$(".holdsInf", ".holdsCav", ".holdsLdr"))).each(function(eltName) {
       Droppables.add($(eltName), {
           constraint:  false,
           dropOnEmpty: true,
           hoverclass:  "okToDrop",
-          onDrop:      function(dragElt, dropElt, evtId) {
-              dragElt.remove();
-              dragElt.setStyle( { left: 0, top: 0 } );
-              dropElt.down("ul").appendChild(dragElt);
-          }
+          onDrop:      function(dragElt, dropElt, evtId) { dropCard(dropElt, dragElt); }
       });
   });
 
-
   $$(".ldr", ".inf", ".cav").each( function(elt) {
-      new Draggable(elt.id, { revert: "failure" } );
+      new Draggable(elt.id, { revert: "failure", zindex: 1500 } );
   });
 
   new Proto.Menu({
       selector: ".inf",
       className: "menu desktop",
       menuItems: [
-          { name: 'Deplete' },
-          { name: 'Restore' }
+          { name: '[-] Deplete' },
+          { name: '[+] Restore' }
       ]
   });
 }
