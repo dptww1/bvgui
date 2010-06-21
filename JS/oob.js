f17748bfebacb26335390d0b07ffec775a6ec638
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2010-06-21 09:35:28 -0400

    new images; some drag-n-drop logic changes

--- JS/oob.js
+++ JS/oob.js
@@ -1,12 +1,12 @@
 function drawCard(side) {
-    var card = Server.drawCard(side);
+    var card = Server.drawCard(side).evalJSON();
     if (card == null) {
         alert("Deck is empty!");
         return;
     }
     var container = $(side + "Hand").down("ul");
     var cardNode = DomUtil.mkCardNode(card);
-    new Draggable(cardNode, { revert: "failure" } );
+    new Draggable(cardNode, { revert: "failure", onStart: createDropTargets, onEnd: clearDropTargets } );  // TODO: combine with fn in domLoaded()
     container.appendChild(cardNode);
     new Effect.BlindDown(cardNode, { duration: 0.5 } );
 }
@@ -53,6 +53,8 @@ function domLoaded() {
       });
   });
 
+  Server.newGame();
+
   $("usaDrawCardLink").href = "javascript:drawCard('usa')";
   $("csaDrawCardLink").href = "javascript:drawCard('csa')";
 
@@ -70,14 +72,21 @@ var DomUtil = function() {
     var _classTemplate = new Template("#{side} #{type}");
 
     var _labelTemplates = {
-        cav: new Template("<img src='Images/cav_#{side}.png' width='18' height='12' border='0'> #{label} C#{str}"),
-        inf: new Template("<img src='Images/inf_#{side}.png' width='18' height='12' border='0'> #{label} I#{str}"),
-        ldr: new Template("<img src='Images/ldr_#{side}.png' width='18' height='12' border='0'> #{label} L#{str}")
+        cav:     new Template("<img src='Images/cav_#{side}.png' width='18' height='12' border='0'> #{name} C#{str}"),
+        inf:     new Template("<img src='Images/inf_#{side}.png' width='18' height='12' border='0'> #{name} I#{str}"),
+        ldr_oc:  new Template("<img src='Images/ldr_#{side}_oc.png' width='18' height='12' border='0'> #{name} L#{str}<ul></ul>"),
+        ldr_sub: new Template("<img src='Images/ldr_#{side}_sub.png' width='18' height='12' border='0'> #{name} L#{str}<ul></ul>"),
+        nav:     new Template("#{name} N#{str}"),
+        map:     new Template("#{name}"),
+        enigma:  new Template("<img src='Images/enigma_#{side}.png' width='18 height='12' border='0'> #{name}")
     };
 
     return {
         mkCardNode : function(card) {
             var t = card.type;
+            if (t === "ldr") {
+                t = card.acap ? "ldr_oc" : "ldr_sub";
+            }
             var n = new Element("li", { "class": _classTemplate.evaluate(card), id: card.id, style: "display:none" });
             n.update(_labelTemplates[t].evaluate(card));
             return n;
@@ -86,45 +95,75 @@ var DomUtil = function() {
 }();
 
 var Server = function() {
-    var usaDeck = [
-        { type: "ldr", side: "usa", id: "U017", str: 1, label: "Buell" },
-        { type: "inf", side: "usa", id: "U040", str: 3, label: "X Corps" }
-    ];
-    var csaDeck = [
-        { type: "cav", side: "csa", id: "C048", str: 1, label: "Van Dorn" }
-    ];
+    var usaCards = [];
+    var csaCards = [];
+
+    var findCard = function(id) {
+        return BVGLOOKUP.Cards[id.charAt(0) == "U" ? "USA" : "CSA"][id];
+    };
 
     var idOf = function(elt) { return elt.id; };
 
     return {
-        drawCard : function(side) { return side == "usa" ? usaDeck.pop() : csaDeck.pop(); },
+        drawCard: function(side) { return Object.toJSON(side == "usa" ? usaCards.pop() : csaCards.pop()); },
+
+        newGame: function() {
+            // Put cards refs in the draw decks.  Don't add cards which are already on the display
+            $H(BVGLOOKUP.Cards.USA).keys().each( function(id) {
+                if ($(id) == null) {
+                    usaCards.push(BVGLOOKUP.Cards.USA[id]);
+                }
+            });
+
+            $H(BVGLOOKUP.Cards.CSA).keys().each( function(id) {
+                if ($(id) == null) {
+                    csaCards.push(BVGLOOKUP.Cards.CSA[id]);
+                }
+            });
+
+            [usaCards, csaCards].each( function(deck) {
+                deck.sort( function(a, b) { return a < b ? 1 : a === b ? 0 : -1; });
+            });
+
+            usaCards = usaCards.reverse();
+            csaCards = csaCards.reverse();
+        },
 
         getDropTargets: function(eltId) {
+            var side = eltId.charAt(0) === "U" ? "usa" : "csa";
+
             var elt = $(eltId);  // TODO: wrap element, shouldn't be using DOM here
 
             var ids = $A();
 
-            var side = elt.hasClassName("csa") ? "csa" : "usa";
-
-            // Generic Containers (TODO: not enabled for Enigmas, Navy)
+            // Generic Containers (TODO: logic is not applicable for Enigmas, Navy!back)
             ids = ids.concat($A(["Hand", "West", "East", "Cadre"]).collect( function(str) { return side + str; } ));
 
-            if (elt.hasClassName("inf")) {
-                // Infantry can go to any active leader
+            switch (findCard(eltId).type) {
+            case "inf":
+                // Infantry can go to any friendly active leader
                 ids = ids.concat($$("." + side + "Active .ldr").collect(idOf));
+                break;
 
-            } else if (elt.hasClassName("cav")) {
+            case "cav":
                 // Cavalry can go to any active top-level leader
                 ids = ids.concat($$("." + side + "Active > .oobRoot > .ldr").collect(idOf));
 
                 // Or to a top-level cavalry
                 ids = ids.concat($$("." + side + "Active > .oobRoot > .cav").collect(idOf));
+                break;
 
-            } else if (elt.hasClassName("ldr")) {
+            case "ldr":
+                break;
 
+            case "nav":
+                break;
+
+            case "enigma":
+                break;
             }
 
-            return ids.compact();
+            return ids;
         }
     };
 }();
